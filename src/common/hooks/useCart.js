14f99c248c07f163cfb35360e807553665b03e98
stephane0713 <stephane.dupre@ynov.com>
2022-03-19 10:39:34 +0100

    added orderIsValid to useCart

--- src/common/hooks/useCart.js
+++ src/common/hooks/useCart.js
@@ -1,5 +1,10 @@
 export default function useCart() {
+  const orderIsValid = ({ qty }) => {
+    return !isNaN(qty) && qty > 1;
+  };
+
   const addToCart = (productId, qty) => {
+    if (!orderIsValid({ qty })) return;
     const cart = JSON.parse(window.localStorage.getItem("cart")) || [];
     const productOrUndefined = cart.find((obj) => productId === obj.id);
     const product = productOrUndefined || { id: productId, qty: 0 };
