3dda14fba8b7c4480cba861097064a6a16d70742
Marie Sommier <marie.a.s45@gmail.com>
2022-03-20 19:21:04 +0100

    navigation wip

--- src/common/components/Navigation.js
+++ src/common/components/Navigation.js
@@ -1,14 +1,65 @@
-//TODO Breadcrumb
-import { styled } from "@mui/system";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 import { Link } from "react-router-dom";
 
-const Navigation = styled("nav")({ border: " 1px solid blue" });
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    right: false,
+  });
 
-export default () => (
-  <Navigation>
-    <Link to="/">Home</Link>
-    <Link to="/products">All products</Link>
-    <Link to="/products/1">1st product</Link>
-    <Link to="/products/7">7th Product</Link>
-  </Navigation>
-);
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+  const urls = [
+    { text: "Home", to: "/" },
+    { text: "Products", to: "/products" },
+    { text: "Product 7", to: "/products/7" },
+  ];
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {urls.map((url, index) => (
+          <Link to={url.to}>
+            <ListItem button key={index}>
+              <ListItemText>{url.text}</ListItemText>
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      {["right"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
