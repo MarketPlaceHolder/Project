361b0ccaa43d3722e5f5306b14be1c0e9840aa17
Marie Sommier <marie.a.s45@gmail.com>
2022-03-20 19:06:01 +0100

    bioutifoul header

--- src/common/components/Header.js
+++ src/common/components/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { styled } from "@mui/system";
+import StoreIcon from "@mui/icons-material/Store";
 
-const Header = styled("header")({ border: "1px solid red" });
+const Header = styled("header")({
+  display: "flex",
+  alignItems: "center",
+  padding: "20px",
+  backgroundColor: "rgb(25, 118, 210)",
+  color: "white",
+  boxShadow: "0 5px 10px black",
+});
 
 export default () => (
   <Header>
-    <h1>MarketPlaceHolder</h1>
+    <StoreIcon></StoreIcon> <h1>MarketPlaceHolder</h1>
   </Header>
 );
