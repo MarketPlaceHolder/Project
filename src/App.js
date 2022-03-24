044dc75b813834c19e9d70ce4ca43ff527a65fe5
stephane-dupre <stephane.dupre@ynov.com>
2022-03-24 18:56:13 +0100

    created cart component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./common/Cart";
 import useCart from "./common/hooks/useCart";
 
 import DetailsPage from "./DetailsPage";
@@ -10,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <CartContext.Provider value={useCart()}>
+        <Cart />
         <Routes>
           <Route path="*" element={<div>Not found</div>} />
           <Route path="/products/:productId" element={<DetailsPage />} />
---
+++ src/common/Cart/index.js
@@ -0,0 +1,31 @@
+import { Badge, Dialog, IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import React, { useContext, useState } from "react";
+import { CartContext } from "../../App";
+
+export default function Cart() {
+  const [open, setOpen] = useState(false);
+  const { cart, nbItems } = useContext(CartContext);
+
+  return (
+    <>
+      <IconButton size="large" color="primary" onClick={() => setOpen(true)}>
+        <Badge badgeContent={nbItems} color="success">
+          <ShoppingCartIcon fontSize="large" />
+        </Badge>
+      </IconButton>
+      <Dialog
+        open={open}
+        onBackdropClick={() => {
+          setOpen(false);
+        }}
+        fullWidth={true}
+        maxWidth={"lg"}
+      >
+        {cart.map((item) => (
+          <div key={item.id}>{item.data.title}</div>
+        ))}
+      </Dialog>
+    </>
+  );
+}
