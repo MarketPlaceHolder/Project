7c4377c08dd28623704b65296c7d2b4f08736ddc
Marie-Bewiwi <marie.a.s45@gmail.com>
2022-03-27 15:32:05 +0200

    button to /products

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import AppCarousel from "./Carousel";
+import { Button } from "@mui/material";
 
 export default function HomePage() {
-  return <AppCarousel />;
+  return (
+    <>
+      <AppCarousel />
+      <Link to="/products">
+        <Button
+          variant="contained"
+          sx={{ display: "block", textAlign: "center", margin: "auto" }}
+        >
+          Voir tous les produits
+        </Button>
+      </Link>
+    </>
+  );
 }
